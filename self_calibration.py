acd7d9edc74e3c3a42051c3f79fda424b265aef6
Maximilian H Abitbol <mabitbol15@gmail.com>
2019-01-14 16:11:29 +0000

    testing

--- self_calibration.py
+++ self_calibration.py
@@ -32,32 +32,33 @@ def calc_eb_var(cmb, so_noise, fsky=0.1):
     return delta_EB_var
 
 def prepare_foregrounds(ells, nu):
-    dust_353 = ld.load_dust(ells)
+    dust_353 = ld.load_dust(ells, EBfrac=0.03)
     dust = ld.scale_dust(dust_353, nu)
     
     # load synch 
     # scale synch
     return dust
 
-def prepare_data(psi0_deg=2., nu=145.):
+def prepare_data(psi0_deg, nu):
     psi0 = psi0_deg * d2r
     cmb_cls, so_noise = prepare_cmb_so()
     dust = prepare_foregrounds(cmb_cls['ells'], nu*1e9)
     obs = full_rotate_cls(psi0, cmb_cls, dust)
-    eb_var = calc_eb_var(obs, so_noise[nu], fsky=0.1)
+    eb_var = calc_eb_var(cmb_cls, so_noise[nu])
+    #eb_var = calc_eb_var(obs, so_noise[nu])
     return cmb_cls, so_noise, dust, eb_var, obs
 
 def eb_likelihood(psis, cmb, obs, eb_var):
     eb_like = []
     for psi in psis:
         numerator = obs['EB'] + 0.5 * sin(4.*psi) * (cmb['EE'] - cmb['BB'])
-        lnlike = -np.sum(numerator**2 / eb_var)
+        lnlike = -np.sum(0.5*numerator**2 / eb_var)
         eb_like.append(lnlike)
     return eb_like
 
 def run_self_calibration(psi0_deg, nu=145.):
-    cmb_cls, so_noise, dust, eb_var, obs = prepare_data(psi0_deg=psi0_deg, nu=145.)
-    psis = np.linspace(psi0_deg-.5, psi0_deg+.5, 1000) * d2r
+    cmb_cls, so_noise, dust, eb_var, obs = prepare_data(psi0_deg=psi0_deg, nu=nu)
+    psis = np.linspace(psi0_deg-2., psi0_deg+2., 1000) * d2r
     eb_like = eb_likelihood(psis, cmb_cls, obs, eb_var)
     likep = eb_like - np.max(eb_like)
     bias = psis[np.where(likep == np.max(likep))[0]][0]
@@ -66,6 +67,19 @@ def run_self_calibration(psi0_deg, nu=145.):
     sigma = psis[y>=0.6827][0] - psis[y<=0.3173][-1]
     return bias, sigma
     
-bias, sigma = run_self_calibration(0)
-print(bias/d2r, sigma/d2r)
+print("\Delta\Psi=0")
+for fnu in SO_freqs:
+    bias, sigma = run_self_calibration(0, fnu)
+    print(fnu, bias/d2r, sigma/d2r)
+
+print("\Delta\Psi=2")
+for fnu in SO_freqs:
+    bias, sigma = run_self_calibration(2., fnu)
+    print(fnu, bias/d2r-2., sigma/d2r)
+
+print("\Delta\Psi=-2")
+for fnu in SO_freqs:
+    bias, sigma = run_self_calibration(-2., fnu)
+    print(fnu, bias/d2r+2., sigma/d2r)
+
 
